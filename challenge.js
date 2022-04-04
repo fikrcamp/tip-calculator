284b08daa69bab128e90fb18b6a7c55bb7a597d9
Asmadhamac <asma.dhamac@gmail.com>
2022-04-04 10:57:35 +0300

    Completed

--- challenge.js
+++ challenge.js
@@ -1,2 +1,12 @@
 // Good Luck! You got this 💪🏾
-// Write your code here.
+
+let bill =300
+let tip1 = bill * 0.15
+let tip2 = bill * 0.2
+
+if(bill >=50 && bill<=300){
+    console.log(`your bill is ${bill} and your tip is ${tip1} and total is ${bill + tip1}`)
+} 
+if(bill >=300){
+    console.log(`your bill is ${bill} and your tip is ${tip2} and total is ${bill + tip2}`)
+}
