6703c8a9723475a8c5040cfc3795761c7c07c790
Mohamed Abdullah <ma3719325@gmail.com>
2022-04-04 11:00:08 +0300

    done

--- challenge.js
+++ challenge.js
@@ -1,2 +1,7 @@
 // Good Luck! You got this 💪🏾
-// Write your code here.
+let bill = 1000
+if(bill>=50 && bill<=300){
+    console.log(`Your bill is ${bill} and your tip is ${bill*0.15} and your total is ${bill+bill*0.15}`)
+}else if (bill>300){
+    console.log(`Your bill is ${bill} and your tip is ${bill*0.2} and your total is ${bill+bill*0.2}`)
+}
