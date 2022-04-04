6c5c92b41601f9dbc3d9532f5e0138e012e993ce
Guleid0 <guleidac@gmail.com>
2022-04-04 13:36:56 +0300

    done

--- challenge.js
+++ challenge.js
@@ -1,2 +1,12 @@
 // Good Luck! You got this 💪🏾
 // Write your code here.
+let bill =300;
+let tip = bill*0.15;
+let total= bill +tip;
+if(bill>=50 && bill <300){
+console.log("your total is  bill plus tip =" + total)
+} else if(bill>=300){
+console.log("your total is bill plus tip = " +(bill + bill*0.2))
+} else{
+    console.log(`your total is bill plus`)
+}
