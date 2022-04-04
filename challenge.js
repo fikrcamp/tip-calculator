47cecb561a1f0d6fc0688eb3db60f42e02ac3caa
melmi4455 <melmi4455@gmail.com>
2022-04-04 23:00:04 +0300

    Assignment Done

--- challenge.js
+++ challenge.js
@@ -1,2 +1,20 @@
 // Good Luck! You got this 💪🏾
 // Write your code here.
+
+
+
+let bill=500
+
+tip1=bill*0.15
+tip2=bill*0.20
+
+
+if (bill>50 && bill<300) {
+    console.log(`The bill is ${bill} , the tip is ${tip1} , and the total is  ${bill + tip1}`)
+}
+
+else if (bill>300) {
+    console.log(`The bill is ${bill} , the tip is ${tip1} , and the total is  ${bill + tip2}`)
+}
+
+
