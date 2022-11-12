a478ff4a6a112587f032243a169bf73becf3e380
AbduleYousuf <abduleyousuf08@gmail.com>
2022-11-12 11:15:04 +0300

    solved the tip challenge

--- challenge.js
+++ challenge.js
@@ -1,2 +1,15 @@
 // Good Luck! You got this 💪🏾
 // Write your code here.
+
+
+let bill = 275;
+
+if (bill > 50 && bill <= 300) {
+    let tip = bill * 0.15
+    console.log("the bill is " + bill + " and the tip was " + tip)
+} else if (bill > 300) {
+    tip = bill * 0.20;
+    console.log("the bill is " + bill + " and the tip was " + tip)
+} else {
+    console.log("the bill is " + bill)
+}
