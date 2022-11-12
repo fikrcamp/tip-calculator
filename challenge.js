80b079f6db3e233e8524347e68944d9eb249c92d
mohan omar <mohaomar338@gmail.com>
2022-11-12 21:15:17 +0300

    rooble

--- challenge.js
+++ challenge.js
@@ -1,2 +1,11 @@
-// Good Luck! You got this 💪🏾
-// Write your code here.
+
+let tip = 0.15
+let bill = 150
+
+    console.log(tip * bill)
+    console.log(`the ${bill} was the bill, the 22.5 was tip, and the total value ${bill+22.5}`)
+
+ 
+
+
+
