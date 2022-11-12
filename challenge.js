c3dd4272c89a2611fa4bada5edd2c8f24886413d
muk7tar <mukhtar.osmna7177@gmail.com>
2022-11-12 13:22:29 +0300

    exercise 3

--- challenge.js
+++ challenge.js
@@ -1,2 +1,17 @@
 // Good Luck! You got this 💪🏾
 // Write your code here.
+
+let bill =275
+if(bill > 50 && bill < 300)
+{
+    let tip =  bill * 0.15
+    let value = bill + tip
+    console.log("The bill was "+bill +", the tip was "+ tip+ ", and the total value " + value)
+
+}
+else{
+    let tip = bill * 0.2
+    let value = bill + tip
+    console.log("The bill was "+bill +", the tip was "+ tip+ ", and the total value " + value)
+}
+
