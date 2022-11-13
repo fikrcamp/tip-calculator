1bec141348ea6d5fbca1e48a8bc91a9ac9646ec9
maxamedkaka <maxamedkaka999@gmail.com>
2022-11-13 20:15:18 +0300

    done js challenge 3

--- challenge.js
+++ challenge.js
@@ -1,2 +1,8 @@
 // Good Luck! You got this 💪🏾
 // Write your code here.
+
+let bill = 200
+let tip = bill * 0.15
+console.log(tip)
+
+console.log(`The bill was ${bill}, the tip was ${tip}, and the total value $230.`)
