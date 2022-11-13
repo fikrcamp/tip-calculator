be0d139f932a6c7e3b4345efe17b1cea5fbf3f5f
AbduleYousuf <abduleyousuf08@gmail.com>
2022-11-13 23:19:20 +0300

    solved again as using arrow function

--- challenge.js
+++ challenge.js
@@ -2,14 +2,15 @@
 // Write your code here.
 
 
-let bill = 275;
-
-if (bill > 50 && bill <= 300) {
-    let tip = bill * 0.15
-    console.log("the bill is " + bill + " and the tip was " + tip)
-} else if (bill > 300) {
-    tip = bill * 0.20;
-    console.log("the bill is " + bill + " and the tip was " + tip)
-} else {
-    console.log("the bill is " + bill)
+const calcTip = (bill) => {
+    totalValue = bill * 0.15 + bill
+    totalValue2 = bill * 0.20 + bill
+    if (bill >= 50 && bill <= 300) {
+        console.log("the bill was " + bill + ", the tip was " + bill * 0.15 + " and the total value is " + totalValue)
+    } else {
+        console.log("the bill was " + bill + ", the tip was " + bill * 0.20 + " and the total value is " + totalValue2)
+    }
+    return bill
 }
+
+calcTip(275)
