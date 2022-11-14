d56343b840f274ee40b1e40efcc3625e5ba24f27
hamda <hamda.baashe44@gmail.com>
2022-11-14 11:39:24 +0000

    finished Js-challenge-3

--- challenge.js
+++ challenge.js
@@ -1,2 +1,22 @@
 // Good Luck! You got this 💪🏾
 // Write your code here.
+
+
+   let bill = 400;
+   let tip = 0;
+    let final = bill + (tip * bill);
+
+     if (bill >= 50 && bill <= 300){
+        tip = 15/100;
+       final = bill + (tip * bill);
+        
+     }
+     else {
+        tip = 20/100;
+        final = bill + (tip * bill)
+       
+     }
+
+console.log(`The bill was ${bill} the tip was ${tip} and the total value is`,final);
+
+
