25630c45eaebe4b71949e0e45aa0e680e8189230
mustafe abdirahman <111310904+carabzl@users.noreply.github.com>
2022-11-14 13:51:49 +0300

    js-challenge-3

--- challenge.js
+++ challenge.js
@@ -1,2 +1,12 @@
 // Good Luck! You got this 💪🏾
 // Write your code here.
+let bill = 275
+let tip1 = bill * 0.15
+let tip2 = bill * 0.20
+let total_v1 = bill + tip1
+let total_v2 = bill + tip2
+if(bill >= 0 && bill <= 300){
+    console.log(`th bill was ${bill}$ the tip was ${tip1}$ and the total value is ${total_v1}$`)
+}else{
+    console.log(`the bill was ${bill}$ the tip was ${tip2}$ and the total value is ${total_v2}$`)
+}
