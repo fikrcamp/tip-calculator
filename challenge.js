194f0fe51a8e3af507cbd4529c3c73b0b0f10adf
Abdilahi mohamed <cabdale3377@gmail.com>
2022-11-14 11:18:50 +0300

    solved tip problem js-challenge-3

--- challenge.js
+++ challenge.js
@@ -1,2 +1,11 @@
-// Good Luck! You got this 💪🏾
-// Write your code here.
+let bill = 200;
+let tip;
+
+if(bill > 50 <=  300){
+  tip = bill * 0.15;
+}else {
+  tip = bill * 0.20;
+}
+
+let total = bill + tip;
+console.log(`The bill was ${bill}, the tip was ${tip} and the total value is ${total}`);
