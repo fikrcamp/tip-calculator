c35c73c2b4d004ed748604b5cff09cf6776f1963
mohamed-hassanjj <mohassanosman@gmail.com>
2022-11-15 07:43:01 +0300

    js-challenge-3

--- challenge.js
+++ challenge.js
@@ -1,2 +1,12 @@
 // Good Luck! You got this 💪🏾
 // Write your code here.
+
+
+let bill = 425
+
+
+if(bill >= 50 && bill <= 300){
+    console("The bill was" + bill + ", and the tip was" + bill * 0.15 )
+}else if (bill > 300){
+    console.log("The bill was" + bill + ", and the tip was" + bill *0.2)
+}
