42672e02ef95cf0f8bcf478fb21630a12bc16359
zayid <sayidka843@gmail.com>
2022-11-15 13:39:48 +0600

    solving lifetime supply exercise

--- challenge.js
+++ challenge.js
@@ -1,2 +1,9 @@
 // Good Luck! You got this 💪🏾
 // Write your code here.
+
+function calculateSupplty(age,amountPerDay){
+    let amountNeed = age*amountPerDay;
+    console.log("You will need " + amountNeed +" to last you until the old age of " +age)
+}
+calculateSupplty(52,2);
+calculateSupplty(23,5);
