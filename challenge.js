14020cabab1dc3336111a9f6c2dd2240cfc14873
mohan omar <mohaomar338@gmail.com>
2022-11-15 18:06:22 +0300

    answered

--- challenge.js
+++ challenge.js
@@ -5,6 +5,16 @@ let bill = 150
     console.log(tip * bill)
     console.log(`the ${bill} was the bill, the 22.5 was tip, and the total value ${bill+22.5}`)
 
+
+    function calctip(billvalue){
+        return 0.15 * billvalue
+
+    }
+   let aishatip = calctip(150)
+
+
+    console.log(aishatip)
+   
  
 
 
