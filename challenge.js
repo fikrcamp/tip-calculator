41d938b76d2c6ddba0694c24390b703fa6d4b91c
Abdalla-10 <abuturab665@gmail.com>
2022-11-17 22:15:32 +0300

    js-challenge-3

--- challenge.js
+++ challenge.js
@@ -1,2 +1,18 @@
 // Good Luck! You got this 💪🏾
 // Write your code here.
+
+//  bill > 290
+//  50 > 300   will be 15%
+//   < 300  will be  20%
+
+
+let bill = 310
+
+if(bill > 50 && bill < 300){
+  let tip = bill * 0.15 
+  console.log(`the was is ${bill} and tip ${tip} the total was ${bill+tip}`)
+}else if(bill=+300){
+    let tip = bill * 0.2 
+    console.log(`the was is ${bill} and tip ${tip} the total was ${bill+tip}`)
+}
+
